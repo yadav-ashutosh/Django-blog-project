2ae2fcdee7b50bc85e0538865a9210452a8fc1e6
ashutosh <ashutoshyadav699@gmail.com>
2020-09-25 02:45:18 +0530

    FInal commit before Delete button

--- blog/views.py
+++ blog/views.py
@@ -6,15 +6,14 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all()
     return render(request, 'blog/post.html', {'posts': posts}) 
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
+            post.author = request.user       
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -35,4 +34,11 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})    
+    return render(request, 'blog/post_edit.html', {'form': form})   
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_drafts.html', {'posts': posts}) 
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)    
